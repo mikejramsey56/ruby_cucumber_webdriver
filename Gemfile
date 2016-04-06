1dceca79e02a2829d2ae205a8fae08ed95f2c60a
mikejramsey56 <mikejramsey56@gmail.com>
2016-04-06 13:16:49 -0400

    Fixed Gemfile, add cucumber.yml

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@
 source "https://rubygems.org"
 
 gem "cucumber", "~>2.3.3"
-gem "rspec-expectations", "~>3.4.0"
+gem "rspec", "~>3.4.0"
 gem "capybara", "~>2.6.2"
 gem "selenium-webdriver", "~>2.53.0"
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,27 @@
 require 'rubygems'
 require 'capybara'
 require 'capybara/dsl'
-require 'rspec'
+require 'capybara/cucumber'
+require 'rspec/expectations'
+require 'selenium-webdriver'
 
 Capybara.run_server = false
-Capybara.default_driver = :selenium
+#Capybara.default_driver = :selenium
+Capybara.default_driver = :firefox
+ Capybara.register_driver :firefox do |app|
+ options = {
+ :js_errors => true,
+ :timeout => 360,
+ :debug => false,
+ :inspector => false,
+ }
+ Capybara::Selenium::Driver.new(app, :browser => :firefox)
+ end
 Capybara.default_selector = :css
 
+=begin
+resynchronize is no longer a sup-port option
+TO-DO: Write wait for AJAX helpers. For jQuery, javascript wait for jQuery.active == 0
 module Helpers
   def without_resynchronize
     page.driver.options[:resynchronize] = false
@@ -14,5 +29,7 @@ module Helpers
     page.driver.options[:resynchronize] = true
   end
 end
+=end
 
-World(Capybara::DSL, Helpers)
+#World(Capybara::DSL, Helpers)
+World(Capybara::DSL)
